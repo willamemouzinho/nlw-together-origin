bcdfdcb5e4a403b56d33a98df4075cc08628ff69
Willame Mouzinho <mouzinhodebritowillame@gmail.com>
2021-11-17 23:06:16 -0300

    Estrutura básica adicionada

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// Executa o script depois que a página carregar
+window.onload = function nameFunction(params) {
+  
+}
+
+// Adiciona evento ao clicar em algo
+let element = document.getElementById("sameId")
+element.addEventListener("click", function(event) {
+  // código
+})
